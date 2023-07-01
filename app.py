faf597f9d8df210168b48faee19131e07543ca79
Zhargal <69998732+Dorzhievzhargal@users.noreply.github.com>
2023-06-30 21:49:41 -0400

    2Th

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from collections.abc import MutableSet
 
 
 app = Flask(__name__)
@@ -101,4 +102,4 @@ def create_article():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False)
